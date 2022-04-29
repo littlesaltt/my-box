eb504c9da4838e2f09f9958850bda379523b6bed
USER <sharon22371173@gmail.com>
2022-04-29 10:44:40 +0800

    midtest box

--- src/com/fju/ai/BoxTester2.java
+++ src/com/fju/ai/BoxTester2.java
@@ -24,22 +24,23 @@ package com.fju.ai;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class BoxTester {
+public class BoxTester2 {
     public static void main(String[] args) {
         List<Box> boxes = new ArrayList<>();
         boxes.add(new Box1());
         boxes.add(new Box2());
         boxes.add(new Box3());
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter object's length: ");
         int length = Integer.parseInt(scanner.next());
         System.out.println("Please enter object's width: ");
         int width = Integer.parseInt(scanner.next());
         System.out.println("Please enter object's height: ");
-        int height = Integer.parseInt(scanner.next());
-
+        int height = Integer.parseInt(scanner.next());*/
+        int length=30;
+        int width=10;
+        int height=20;
         for (Box box : boxes) {
             if (box.isFit(length, width, height)) {
                 System.out.println(box);
